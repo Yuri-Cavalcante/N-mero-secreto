423c63d8762a276134f60ad5a8d92af74a35d0cd
Yuri Costa <yuric.cavalcante3@gmail.com>
2025-05-29 19:08:24 -0300

    habilidades implementadas

--- script.js
+++ script.js
@@ -1,39 +1,133 @@
-// gerador de numero 
+// GERADOR DE NÚMERO  
 let numeroGerado = parseInt(Math.random() * (101 - 1) + 1);
+console.log(numeroGerado);
 
-// utilitarios
+// UTILITÁRIOS
 const pegaElemento = (id) => document.getElementById(id);
 
-function telaDerrota() {
-    const tela = document.getElementById('gameOver');
-    tela.style.display = "flex"
+let condicaoVitoria = false;
+
+function verificaJogo() {
+    if (vidas <= 0) {
+        telaDerrota();
+        sombraTela();
+    } else {
+        if (condicaoVitoria == true) {
+            telaVitoria();
+            sombraTela();
+        }
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const botaoRecomecar = pegaElemento("recomecar");
-    botaoRecomecar.addEventListener("click", recomeca);
-    function recomeca() {
-        location.reload();
-    }
-})
+function telaVitoria() {
+    pegaElemento('gameOver').id = 'telaVitoria';
+    const tela = pegaElemento('telaVitoria');
+    tela.style.display = "flex";
+    pegaElemento('mostradorTelaFinal').textContent = 'VITÓRIA';
+}
 
-function SomTelaDer() {
+function telaDerrota() {
+    const tela = pegaElemento('gameOver');
+    tela.style.display = "flex";
+}
+
+function recomeca() {
+    location.reload();
+}
+
+function sombraTela() {
     const escurece =  pegaElemento('sombraTelaDeDerrota');
     escurece.style.filter = 'brightness(0.5)';
 }
 
-// mostrador de vida 
-let vidas = 8;
+// MOSTRADOR VIDA
+let vidas = 7;
 pegaElemento('pontosVida').textContent = `Vidas: ${vidas}`;
 
-//função principal
+//HABILIDADES 
+
+let pontos = 1000 
+
+function faca(botao) {
+    let antecessor = numeroGerado - 15;
+    if (antecessor  <= 0) {
+        antecessor = (antecessor * -1) + antecessor + 1;
+    }
+    let sucessor = numeroGerado + 15;
+    if (sucessor > 100) {
+        sucessor = sucessor - (sucessor - 100) ;
+    }
+
+    numeroGerado = parseInt(Math.random() * (sucessor - antecessor) + antecessor);
+    console.log(antecessor);
+    console.log(sucessor);
+    console.log(numeroGerado);
+
+    vidas -= 2;
+    pegaElemento('pontosVida').textContent = `Vidas: ${vidas}`;
+    pegaElemento('mostrador').textContent = ` Seu número está entre ${antecessor} e ${sucessor}`;
+    verificaJogo();
+    botao.disabled = true;
+}
+
+function coringa(botao) {
+    let antecessor = numeroGerado - 1;
+    
+    let sucessor = numeroGerado + 1; 
+      
+    vidas = 1;
+
+    switch(true) {
+        case numeroGerado == 1:
+            pegaElemento('pontosVida').textContent = `Vidas: ${vidas}`;
+            pegaElemento('mostrador').textContent = `Seu numero pode ser ${numeroGerado} ou ${sucessor}`
+            break;
+        case numeroGerado == 100:
+            pegaElemento('pontosVida').textContent = `Vidas: ${vidas}`;
+            pegaElemento('mostrador').textContent = `Seu numero pode ser ${antecessor} ou ${numeroGerado}`
+            break;
+        default:
+            
+            pegaElemento('pontosVida').textContent = `Vidas: ${vidas}`;
+            pegaElemento('mostrador').textContent = ` Seu número pode ser ${antecessor}, ${numeroGerado} ou ${sucessor}`;   
+    }
+
+    let opcoes = [antecessor, numeroGerado, sucessor];
+
+    numeroGerado = opcoes[Math.floor(Math.random() * opcoes.length)];
+
+    console.log(antecessor);
+    console.log(numeroGerado);
+    console.log(sucessor);
+    
+    botao.disabled = true;
+}
+
+function trevo(botao) {
+    pegaElemento('mostrador').textContent = 'Mais uma vida, use bem.'
+
+    vidas += 1;
+
+    pegaElemento('pontosVida').textContent = vidas;
+
+    if (Math.random() < 0.1) {
+        numeroGerado = parseInt(Math.random() * (101 - 1) + 1);
+        pegaElemento('mostrador').textContent = 'O número mudou, mas que azar.'
+        console.log(numeroGerado);    
+    }
+    
+    botao.disabled = true
+}
+
+//CHUTAR NUMERO 
 
 function chutarNumero () {
     let numeroEntrada = document.getElementById('numeroChute').value;
-        
+
     switch(true) {
         case numeroEntrada == numeroGerado:
             pegaElemento('mostrador').textContent = 'Você acertou!'
+            condicaoVitoria = true;
             break;
         case numeroEntrada < numeroGerado && numeroEntrada <= 100 && numeroEntrada >= 1:
             pegaElemento('mostrador').textContent = 'Você errou, o número é maior.'
@@ -49,13 +143,9 @@ function chutarNumero () {
             alert('o numero não pode ser negativo ou o campo estar vazio, e deve ser entre 1 e 100!');
     }
 
-    if (vidas == 0) {
-        telaDerrota();
-        SomTelaDer();
-    }
-    
-    console.log(numeroGerado);
+    verificaJogo();
 }
+
 
 
 //1. gerar numero aletorio  de 1 a 100 
@@ -66,39 +156,4 @@ function chutarNumero () {
 //6. Caso o num de vidas chegar a 0, mostrar tela de derrota, o numero secreto e o botão de recomeçar 
 //7. criar repositorio com os arquivos no github
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ESTRUTURA DE REPETIÇÃO COM GERAÇÃO DE NUMERO ALEATORIO 
-
-// let alvo = 1;
-// let tentativa;
-
-
-// do {
-//     tentativa = Math.floor(Math.random() * (101 - 1) + 1);
-//     console.log(`Número gerado ${tentativa}`);
-// } while (tentativa !== alvo);
